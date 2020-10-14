1d5a3812c647b39a75f6b2db837ae1a2d664eddd
Devon <dmryan711@gmail.com>
2020-10-13 23:52:49 -0400

    Implemented routing and tabs

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {IonPage, IonHeader, IonToolbar, IonTitle} from "@ionic/react";
+
+const Home = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Hunt</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+        </IonPage>
+    )
+}
+
+export default Home;
