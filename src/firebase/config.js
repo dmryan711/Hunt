8f38c7f2302366060bc4263a4519204d0f5d93ec
Devon <dmryan711@gmail.com>
2020-10-15 14:40:59 -0400

    Added base Firebase arch

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlM3-haY1P2aHCK_Sd-r6JFr7RuPCKnPg",
+    authDomain: "hunt-88e93.firebaseapp.com",
+    databaseURL: "https://hunt-88e93.firebaseio.com",
+    projectId: "hunt-88e93",
+    storageBucket: "hunt-88e93.appspot.com",
+    messagingSenderId: "214647691949",
+    appId: "1:214647691949:web:3df67fbc3a48f8abf24424"
+  };
+
+export default firebaseConfig;
